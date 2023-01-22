9b5fc133747f646fb1612dd951b3295cc13099c6
Ricard Roberg <ricardroberg@gmail.com>
2023-01-22 09:53:24 -0300

    Aula4 - mobile

---
+++ mobile/src/screens/Habit.tsx
@@ -0,0 +1,44 @@
+import { View, Text, ScrollView } from "react-native";
+import { useRoute } from "@react-navigation/native";
+import dayjs from "dayjs";
+
+import { BackButton } from "../componentes/BackButton";
+import ProgressBar from "../componentes/ProgressBar";
+import Checkbox from "../componentes/Checkbox";
+
+interface Params {
+  date: string;
+}
+
+export function Habit() {
+  const route = useRoute();
+  const { date } = route.params as Params;
+
+  const parseDate = dayjs(date);
+  const dayOfWeek = parseDate.format("dddd");
+  const dayAndMonth = parseDate.format("DD/MM");
+
+  return (
+    <View className="flex-1 bg-background px-8 pt-16">
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <BackButton />
+
+        <Text className="mt-6 text-zinc-400 font-semibold text-base lowercase">
+          {dayOfWeek}
+        </Text>
+        <Text className="text-white font-extrabold text-3xl">
+          {dayAndMonth}
+        </Text>
+        <ProgressBar progress={80} />
+
+        <View className="mt-6">
+          <Checkbox title="Beber 2L de água" checked={false} />
+          <Checkbox title="Caminhar" checked/>
+        </View>
+      </ScrollView>
+    </View>
+  );
+}
---
+++ mobile/src/utils/generate-range-between-dates.ts
@@ -0,0 +1,15 @@
+import dayjs from "dayjs";
+
+export function generateDatesFromYearBeginning() {
+  const startDate = dayjs().startOf("year");
+  const endDate = new Date();
+
+  const dateRange = [];
+  let compareDate = startDate;
+
+  while (compareDate.isBefore(endDate)) {
+    dateRange.push(compareDate.toDate());
+    compareDate = compareDate.add(1, "day");
+  }
+  return dateRange;
+}
